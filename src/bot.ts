c22f8d38f252f6c66839eb436a96cb861356f9ac
Hazmi35 <contact@hzmi.xyz>
2020-04-04 00:29:09 +0700

    Some updates

--- src/bot.ts
+++ src/bot.ts
@@ -8,8 +8,9 @@ const client = new Client({ disableMentions: "everyone" })
 client.on("ready", () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore-next-line (I don't know why ts-node when using pnpm error on this line but tsc does not)
-    client.log.info(`I'm ready to serve ${client.users.cache.size} users on ${client.guilds.cache.size} guilds!`);
+    client.log.info(`[Shard #${client.shard!.ids}] I'm ready to serve ${client.users.cache.size} users on ${client.guilds.cache.size} guilds!`);
     client.user!.setPresence({ activity: { name: "Hello There!", type: "PLAYING" }, afk: false });
+    // client.destroy();
 });
 
 client.on("warn", client.log.warn);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import { config } from "dotenv"; config();
+import { resolve } from "path";
 import { ShardingManager } from "discord.js";
 import { LogWrapper } from "./utils/LogWrapper";
 import { totalShards, name } from "./config.json";
 
 const log = new LogWrapper(name + "-sharding").logger;
 
-new ShardingManager("./bot", { totalShards: totalShards as number | "auto", mode: "worker", respawn: true })
+new ShardingManager(resolve(__dirname, "bot.js"), { totalShards: totalShards as number | "auto", mode: "worker", respawn: true, token: process.env.DISCORD_TOKEN })
     .on("shardCreate", (shard) => {
-        log.info(`[shardCreate] Spawned shard: ${shard.id}`);
+        log.info(`Shard #${shard.id}: Spawned.`);
+        shard.on("disconnect", () => {
+            log.info(`Shard #${shard.id}: Disconnected`);
+            // log.warn({ message: `Shard #${shard.id}: Disconnected` }); // TODO: Fix this.
+        }).on("reconnecting", () => {
+            log.info(`Shard #${shard.id}: Reconnected.`);
+        });
     }).spawn(totalShards as number | "auto");
