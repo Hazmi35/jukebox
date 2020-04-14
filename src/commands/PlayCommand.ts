4251d9f736fe94669520462db6fc47ebf49cd98d
Hazmi35 <contact@hzmi.xyz>
2020-04-14 15:45:11 +0700

    Fix bot can't play livestreams

--- src/commands/PlayCommand.ts
+++ src/commands/PlayCommand.ts
@@ -142,7 +142,7 @@ export default class PlayCommand extends BaseCommand {
         serverQueue.connection!.voice.setSelfDeaf(true);
         // This fixes: https://github.com/discordjs/discord.js/issues/4027, highWatermark option is will be increased or decreased in the future
         // eslint-disable-next-line no-bitwise
-        const dispatcher = guild.queue!.connection!.play(await ytdl(song.url, { filter: "audioonly", highWaterMark: 1<<25 }), { type: "opus" })
+        const dispatcher = guild.queue!.connection!.play(await ytdl(song.url, { filter: "audioonly", highWaterMark: 1<<26 }), { type: "opus" })
             .on("start", () => {
                 serverQueue.playing = true;
                 this.client.log.info(`${this.client.shard ? `[Shard #${this.client.shard.ids}]` : ""} Song: "${song.title}" on ${guild.name} started`);
