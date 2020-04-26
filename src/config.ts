489d47193f24773469cd5cc805f5512f77bc5d2c
Hazmi35 <contact@hzmi.xyz>
2020-04-26 22:44:36 +0700

    Update NodeJS CI

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const name: string = process.env.CONFIG_NAME!;
-export const prefix = process.env.CONFIG_PREFIX!.replace(/"/g, ""); // FIX: Temporary workaround for https://github.com/docker/compose/issues/6951
+export const prefix = process.env.CONFIG_PREFIX!.replace(/"/g, ""); // Temporary workaround for https://github.com/docker/compose/issues/6951
 export const owners: string[] = JSON.parse(process.env.CONFIG_OWNERS!);
 export const totalShards: string | number = process.env.CONFIG_TOTALSHARDS!;
 export const defaultVolume = Number(process.env.CONFIG_DEFAULT_VOLUME);
