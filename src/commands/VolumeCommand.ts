eebbc06e95294efb9829a8d7ed61ac1d2027d14f
Hazmi35 <contact@hzmi.xyz>
2020-04-27 21:14:38 +0700

    command(volume): Handle when user set volume below zero

--- src/commands/VolumeCommand.ts
+++ src/commands/VolumeCommand.ts
@@ -13,15 +13,17 @@ export default class VolumeCommand extends BaseCommand {
         });
     }
     public execute(message: IMessage, args: string[]): any {
+        let volume = Number(args[0]);
         if (!message.member!.voice.channel) return message.channel.send(new MessageEmbed().setDescription("You're not in a voice channel").setColor("#FFFF00"));
         if (!message.guild!.queue) return message.channel.send(new MessageEmbed().setDescription("There is nothing playing.").setColor("#FFFF00"));
         if (message.member!.voice.channel.id !== message.guild!.queue.voiceChannel!.id) return message.channel.send(
             new MessageEmbed().setDescription("You need to be in the same voice channel as mine").setColor("#FF0000"));
+        if (volume < 0) volume = 0;
 
-        if (isNaN(Number(args[0]))) return message.channel.send(
-            new MessageEmbed().setDescription(`📶 The current volume is ${message.guild!.queue.volume}`).setColor("#00F00"));
-        if (Number(args[0]) > this.client.config.maxVolume) return message.channel.send(
-            new MessageEmbed().setDescription(`Can't set the volume above ${this.client.config.maxVolume}`).setColor("#FFFF00"));
+        if (isNaN(volume)) return message.channel.send(new MessageEmbed().setDescription(`📶 The current volume is ${message.guild!.queue.volume}`).setColor("#00FF00"));
+        if (volume === 0) return message.channel.send(new MessageEmbed().setDescription("🔇 Muting song...").setColor("#00FF00"));
+        if (Number(args[0]) > this.client.config.maxVolume) return message.channel.send(new MessageEmbed()
+            .setDescription(`❗ Can't set the volume above ${this.client.config.maxVolume}`).setColor("#FFFF00"));
 
         message.channel.send(new MessageEmbed().setDescription(`📶 Volume set to ${args[0]}`).setColor("#00FF00"));
         message.guild!.queue.volume = Number(args[0]);
