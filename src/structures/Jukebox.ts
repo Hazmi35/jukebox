5ca549a591b67c79140bbbb1887f2821ad9bcf48
Hazmi35 <contact@hzmi.xyz>
2020-06-22 00:17:23 +0700

    feat(cache): Add warning when user enables cacheYoutubeDownloads

--- src/structures/Jukebox.ts
+++ src/structures/Jukebox.ts
@@ -22,6 +22,8 @@ export default class Jukebox extends Client {
     constructor(opt: ClientOptions) { super(opt); }
 
     public async build(): Promise<Jukebox> {
+        // NOTE: Will be removed when caching is not a experimental feature anymore
+        if (this.config.cacheYoutubeDownloads) this.log.warn("cacheYoutubeDownloads is still a experimental feature");
         this.on("ready", () => this.commandsHandler.load());
         this.eventsLaoder.load();
         await this.login(this.getToken());
