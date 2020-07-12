9918406e69147e6b3d27945b99f9e5bbd4731a01
Hazmi35 <contact@hzmi.xyz>
2020-07-13 04:46:15 +0700

    feat(caching): Some improvements
    * Add song length limiter for cache
    * Fix #81 by using a finish marker

--- src/commands/PlayCommand.ts
+++ src/commands/PlayCommand.ts
@@ -154,7 +154,7 @@ export default class PlayCommand extends BaseCommand {
         }
 
         serverQueue.connection!.voice.setSelfDeaf(true);
-        const SongData = await ytdl(song.url, { cache: this.client.config.cacheYoutubeDownloads });
+        const SongData = await ytdl(song.url, { cache: this.client.config.cacheYoutubeDownloads, cacheMaxLength: this.client.config.cacheMaxLengthAllowed });
 
         if (SongData.cache) this.client.log.info(`${this.client.shard ? `[Shard #${this.client.shard.ids}]` : ""} Using cache for song "${song.title}" on ${guild.name}`);
 
