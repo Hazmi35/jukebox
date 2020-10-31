ea067c3341da0105cbf306ec442e422fea4f4f15
hzmi <contact@hzmi.xyz>
2020-11-01 06:06:48 +0700

    feat(Presence): Custom status data (#212)

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,19 @@
+import type { ActivityType } from "discord.js";
+
 export const name: string = process.env.CONFIG_NAME ?? "Jukebox";
 export const prefix = process.env.CONFIG_PREFIX?.replace(/"/g, "") ?? "jukebox "; // Temporary workaround for https://github.com/docker/compose/issues/6951
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 export const owners: string[] = process.env.CONFIG_OWNERS?.replace(/  +/g, " ").split(/,[ ]?/) ?? [];
 export const totalShards: string | number = process.env.CONFIG_TOTALSHARDS ?? "auto";
 export const defaultVolume = Number(process.env.CONFIG_DEFAULT_VOLUME) || 100;
 export const maxVolume = Number(process.env.CONFIG_MAX_VOLUME) || 100;
-export const allowDuplicate: boolean = process.env.CONFIG_ALLOW_DUPLICATE! === "yes" || false;
+export const allowDuplicate: boolean = process.env.CONFIG_ALLOW_DUPLICATE === "yes";
 export const deleteQueueTimeout = Number(process.env.CONFIG_DELETE_QUEUE_TIMEOUT) * 1000 || 180 * 1000;
-export const cacheYoutubeDownloads: boolean = process.env.CONFIG_CACHE_YOUTUBE_DOWNLOADS === "yes" || false;
+export const cacheYoutubeDownloads: boolean = process.env.CONFIG_CACHE_YOUTUBE_DOWNLOADS === "yes";
 export const cacheMaxLengthAllowed = Number(process.env.CONFIG_CACHE_MAX_LENGTH) || 5400;
-export const disableInviteCmd = process.env.CONFIG_DISABLE_INVITE_CMD === "yes" || false;
-export const debug = process.env.CONFIG_DEBUG === "yes" || false;
+export const disableInviteCmd = process.env.CONFIG_DISABLE_INVITE_CMD === "yes";
+export const debug = process.env.CONFIG_DEBUG === "yes";
+export const status = {
+    type: process.env.STATUS_TYPE?.toUpperCase() as ActivityType | null ?? "LISTENING",
+    activity: process.env.CONFIG_STATUS_ACTIVITY ?? "music on {guildsCount}"
+};
+export const fetchAllUsers = process.env.CONFIG_FETCH_ALL_USERS === "yes";
