ed2721014a18f73cd361067bf49ef3a58c31fb2b
Hazmi35 <contact@hzmi.xyz>
2020-11-08 02:28:43 +0700

    feat(ListenerLoader): use decorator

--- src/listeners/ErrorEvent.ts
+++ src/listeners/ErrorEvent.ts
@@ -1,9 +1,10 @@
 import { ClientEventListener } from "../../typings";
 import Jukebox from "../structures/Jukebox";
+import { DefineListener } from "../utils/decorators/DefineListener";
 
+@DefineListener("error")
 export default class ErrorEvent implements ClientEventListener {
-    public readonly name = "error";
-    public constructor(private readonly client: Jukebox) {}
+    public constructor(private readonly client: Jukebox, public name: ClientEventListener["name"]) {}
 
     public execute(error: string): void {
         this.client.logger.error("CLIENT_ERROR:", error);
--- src/listeners/WarnEvent.ts
+++ src/listeners/WarnEvent.ts
@@ -1,9 +1,10 @@
 import { ClientEventListener } from "../../typings";
 import Jukebox from "../structures/Jukebox";
+import { DefineListener } from "../utils/decorators/DefineListener";
 
+@DefineListener("message")
 export default class WarnEvent implements ClientEventListener {
-    public readonly name = "warn";
-    public constructor(private readonly client: Jukebox) {}
+    public constructor(private readonly client: Jukebox, public name: ClientEventListener["name"]) {}
 
     public execute(warn: string): void {
         this.client.logger.warn("CLIENT_WARN:", warn);
