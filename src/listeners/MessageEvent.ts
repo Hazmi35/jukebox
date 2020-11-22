8db0b1c22431c3ca1836b959977746cf6ee1af9d
Hazmi35 <contact@hzmi.xyz>
2020-11-22 10:37:25 +0700

    feat(MessageEvent): only listen to text channels

--- src/listeners/MessageEvent.ts
+++ src/listeners/MessageEvent.ts
@@ -7,8 +7,7 @@ import { User } from "discord.js";
 @DefineListener("message")
 export class MessageEvent extends BaseListener {
     public async execute(message: IMessage): Promise<any> {
-        if (message.author.bot) return message;
-        if (message.channel.type === "dm") return message;
+        if (message.author.bot || message.channel.type !== "text") return message;
         if ((await this.getUserFromMention(message.content))?.id === message.client.user?.id) {
             return message.channel.send(
                 createEmbed("info", `Hi, I'm a simple music bot, see my commands with \`${this.client.config.prefix}help\``)
