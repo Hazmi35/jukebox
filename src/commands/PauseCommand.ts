e39b3f4abe3b4a698ec6df38b4a69653cd3d9901
zarrttt <undefinedgaming01@gmail.com>
2021-01-24 21:34:12 +0700

    feat(PauseCommand): The music player has been paused!, Music player is already paused!

--- src/commands/PauseCommand.ts
+++ src/commands/PauseCommand.ts
@@ -17,9 +17,9 @@ export class PauseCommand extends BaseCommand {
         if (message.guild?.queue?.playing) {
             message.guild.queue.playing = false;
             message.guild.queue.connection?.dispatcher.pause();
-            return message.channel.send(createEmbed("info", "⏸ Paused the music for you!"));
+            return message.channel.send(createEmbed("info", "⏸ The music player has been paused!"));
         }
-        message.channel.send(createEmbed("warn", "❗ Music is already paused!"))
+        message.channel.send(createEmbed("warn", "❗ Music player is already paused!"))
             .catch(e => this.client.logger.error("PAUSE_CMD_ERR:", e));
     }
 }
