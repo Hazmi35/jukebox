8bc6d7afff9e2aae2077a0fdd33a4efdbe87ca75
mzrtamp <40928387+mzrtamp@users.noreply.github.com>
2021-01-27 16:37:55 +0700

    fix(ResumeCommand): fix grammaring
    Co-authored-by: hzmi <contact@hzmi.xyz>

--- src/commands/ResumeCommand.ts
+++ src/commands/ResumeCommand.ts
@@ -19,7 +19,7 @@ export class ResumeCommand extends BaseCommand {
         } else {
             message.guild!.queue!.playing = true;
             message.guild?.queue?.connection?.dispatcher.resume();
-            message.channel.send(createEmbed("info", "▶ Resumed the music player!")).catch(e => this.client.logger.error("RESUME_CMD_ERR:", e));
+            message.channel.send(createEmbed("info", "▶ The music player resumed")).catch(e => this.client.logger.error("RESUME_CMD_ERR:", e));
         }
     }
 }
