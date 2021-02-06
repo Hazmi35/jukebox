cb4a39eb2ec96559c6ab7eefe36e76779531e1ca
Hazmi35 <contact@hzmi.xyz>
2021-02-06 18:27:08 +0700

    fix(YouTubeAPI): Throw null if there is no available thumbnails on playlist too

--- src/utils/YouTube/structures/Playlist.ts
+++ src/utils/YouTube/structures/Playlist.ts
@@ -17,7 +17,7 @@ export class Playlist extends Item {
         this.itemCount = type === "api" ? (rawData as APIPlaylist).itemCount : (rawData as SRPlaylist).videoCount;
 
         this.thumbnailURL = type === "api"
-            ? (rawData as APIPlaylist).thumbnailURL
+            ? (rawData as APIPlaylist).thumbnailURL!
             : (rawData as SRPlaylist).thumbnail! as unknown as string;
     }
 
