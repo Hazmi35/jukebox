18c46bd9c846477d4387a4e5438c5a5417e77349
Hazmi35 <contact@hzmi.xyz>
2021-05-03 07:57:31 +0700

    refactor(QueueCommand): Refactor message collector

--- src/commands/QueueCommand.ts
+++ src/commands/QueueCommand.ts
@@ -3,6 +3,7 @@ import { IMessage } from "../../typings";
 import { DefineCommand } from "../utils/decorators/DefineCommand";
 import { isMusicPlaying } from "../utils/decorators/MusicHelper";
 import { createEmbed } from "../utils/createEmbed";
+import { TextChannel } from "discord.js";
 
 @DefineCommand({
     aliases: ["q"],
@@ -23,21 +24,31 @@ export class QueueCommand extends BaseCommand {
             const indexes: string[] = this.chunk(songs!, 12);
             let index = 0;
             embed.setDescription(indexes[index]).setFooter(`Page ${index + 1} of ${indexes.length}`, "https://raw.githubusercontent.com/Hazmi35/jukebox/main/.github/images/info.png");
+            const reactions = ["◀️", "▶️"];
             message.channel.send(embed).then(msg => {
                 msg.react("◀️").then(() => {
                     msg.react("▶️").catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
-                    msg.createReactionCollector((reaction, user) => reaction.emoji.name === "◀️" && user.id === message.author.id, { time: 80 * 1000 }).on("collect", () => {
-                        if (index === 0) return undefined;
-                        index--;
-                        embed.setDescription(indexes[index]).setFooter(`Page ${index + 1} of ${indexes.length}`, "https://raw.githubusercontent.com/Hazmi35/jukebox/main/.github/images/info.png");
-                        msg.edit(embed).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
-                    });
-                    msg.createReactionCollector((reaction, user) => reaction.emoji.name === "▶️" && user.id === message.author.id, { time: 80 * 1000 }).on("collect", () => {
-                        if (index + 1 === indexes.length) return undefined;
-                        index++;
-                        embed.setDescription(indexes[index]).setFooter(`Page ${index + 1} of ${indexes.length}`, "https://raw.githubusercontent.com/Hazmi35/jukebox/main/.github/images/info.png");
-                        msg.edit(embed).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
-                    });
+                    const isMessageManageable = (msg.channel as TextChannel).permissionsFor(msg.client.user!)?.has("MANAGE_MESSAGES");
+                    msg.createReactionCollector((reaction, user) => reactions.includes(reaction.emoji.name) && user.id === message.author.id, { time: 80 * 1000 })
+                        .on("collect", (reaction, user) => {
+                            if (isMessageManageable) reaction.users.remove(user).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
+                            switch (reaction.emoji.name) {
+                                case "◀️":
+                                    if (index === 0) return undefined;
+                                    index--;
+                                    break;
+
+                                case "▶️":
+                                    if (index + 1 === indexes.length) return undefined;
+                                    index++;
+                                    break;
+                            }
+                            embed.setDescription(indexes[index]).setFooter(`Page ${index + 1} of ${indexes.length}`, "https://raw.githubusercontent.com/Hazmi35/jukebox/main/.github/images/info.png");
+                            msg.edit(embed).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
+                        })
+                        .on("end", () => {
+                            if (isMessageManageable) msg.reactions.removeAll().catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
+                        });
                 }).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
             }).catch(e => this.client.logger.error("QUEUE_CMD_ERR:", e));
         } else {
