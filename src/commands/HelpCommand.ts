65c8e95c6f293dc48a471e360b226a3426b63d07
Hazmi35 <contact@hzmi.xyz>
2021-08-11 02:01:20 +0700

    refactor(HelpCommand): Use createEmbed

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -1,5 +1,5 @@
 import { BaseCommand } from "../structures/BaseCommand";
-import { Message, MessageEmbed } from "discord.js";
+import { Message } from "discord.js";
 import { DefineCommand } from "../utils/decorators/DefineCommand";
 import { createEmbed } from "../utils/createEmbed";
 
@@ -15,20 +15,18 @@ export class HelpCommand extends BaseCommand {
             message.client.commands.get(message.client.commands.aliases.get(args[0])!);
         if (command && !command.meta.disable) {
             message.channel.send(
-                new MessageEmbed()
+                createEmbed("info")
                     .setTitle(`Information for the ${command.meta.name} command`)
                     .setThumbnail("https://raw.githubusercontent.com/Hazmi35/jukebox/main/.github/images/question_mark.png")
                     .addFields({ name: "Name", value: `\`${command.meta.name}\``, inline: true },
                         { name: "Description", value: command.meta.description, inline: true },
                         { name: "Aliases", value: `${Number(command.meta.aliases?.length) > 0 ? command.meta.aliases?.map(c => `\`${c}\``).join(", ") as string : "None."}`, inline: true },
                         { name: "Usage", value: `\`${command.meta.usage?.replace(/{prefix}/g, message.client.config.prefix) as string}\``, inline: false })
-                    .setColor("#00FF00")
             ).catch(e => this.client.logger.error("HELP_CMD_ERR:", e));
         } else {
             message.channel.send(
                 createEmbed("info", message.client.commands.filter(cmd => !cmd.meta.disable && cmd.meta.name !== "eval").map(c => `\`${c.meta.name}\``).join(" "))
                     .setTitle("Help Menu")
-                    .setColor("#00FF00")
                     .setThumbnail(message.client.user?.displayAvatarURL() as string)
                     .setFooter(`Use ${message.client.config.prefix}help <command> to get more info on a specific command!`, "https://raw.githubusercontent.com/Hazmi35/jukebox/main/.github/images/info.png")
             ).catch(e => this.client.logger.error("HELP_CMD_ERR:", e));
