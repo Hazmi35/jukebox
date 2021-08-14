a9f514a71579d5d11622a6bc98a20dd86cf68c02
Hazmi35 <contact@hzmi.xyz>
2021-08-14 17:24:46 +0700

    feat(MessageEvent): Migrate to Discord.js v13

--- src/events/MessageEvent.ts
+++ src/events/MessageEvent.ts
@@ -6,14 +6,14 @@ import { Message } from "discord.js";
 @DefineListener("message")
 export class MessageEvent extends BaseListener {
     public async execute(message: Message): Promise<any> {
-        if (message.author.bot || message.channel.type !== "text") return message;
+        if (message.author.bot || message.channel.type !== "GUILD_TEXT") return message;
 
         if (message.content.toLowerCase().startsWith(this.client.config.prefix)) return this.client.commands.handle(message);
 
         if ((await this.client.util.getUserFromMention(message.content))?.id === message.client.user?.id) {
-            return message.channel.send(
-                createEmbed("info", `Hi, I'm a simple music bot, see my commands with \`${this.client.config.prefix}help\``)
-            );
+            return message.channel.send({
+                embeds: [createEmbed("info", `Hi, I'm a simple music bot, see my commands with \`${this.client.config.prefix}help\``)]
+            });
         }
     }
 }
