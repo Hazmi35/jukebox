7123ad4d8706c7edfb2c7f158733512b031fe1db
Hazmi35 <contact@hzmi.xyz>
2021-08-16 19:33:20 +0700

    feat(MusicHelper): Migrate to Discord.js v13

--- src/utils/decorators/MusicHelper.ts
+++ src/utils/decorators/MusicHelper.ts
@@ -1,9 +1,10 @@
 import { Inhibit } from "./Inhibit";
 import { createEmbed } from "../createEmbed";
+import { VoiceChannel } from "discord.js";
 
 export function isMusicQueueExists(): any {
     return Inhibit(message => {
-        if (message.guild?.queue === null) return message.channel.send(createEmbed("warn", "There is nothing playing."));
+        if (message.guild?.queue === null) return message.channel.send({ embeds: [createEmbed("warn", "There is nothing playing.")] });
     });
 }
 
@@ -12,9 +13,9 @@ export function isSameVoiceChannel(): any {
         if (!message.guild?.me?.voice.channel) return undefined;
         const botVoiceChannel = message.guild.queue?.voiceChannel?.id ?? message.guild.me.voice.channel.id;
         if (message.member?.voice.channel?.id !== botVoiceChannel) {
-            return message.channel.send(
-                createEmbed("warn", "You need to be in the same voice channel as mine")
-            );
+            return message.channel.send({
+                embeds: [createEmbed("warn", "You need to be in the same voice channel as mine")]
+            });
         }
     });
 }
@@ -22,9 +23,9 @@ export function isSameVoiceChannel(): any {
 export function isUserInTheVoiceChannel(): any {
     return Inhibit(message => {
         if (!message.member?.voice.channel) {
-            return message.channel.send(
-                createEmbed("warn", "I'm sorry, but you need to be in a voice channel to do that")
-            );
+            return message.channel.send({
+                embeds: [createEmbed("warn", "I'm sorry, but you need to be in a voice channel to do that")]
+            });
         }
     });
 }
@@ -34,11 +35,10 @@ export function isValidVoiceChannel(): any {
         const voiceChannel = message.member?.voice.channel;
         if (voiceChannel?.id === message.guild?.me?.voice.channel?.id) return undefined;
         if (!voiceChannel?.joinable) {
-            return message.channel.send(createEmbed("error", "I'm sorry, but I can't connect to your voice channel, make sure I have the proper permissions!"));
+            return message.channel.send({ embeds: [createEmbed("error", "I'm sorry, but I can't connect to your voice channel, make sure I have the proper permissions!")] });
         }
-        if (!voiceChannel.speakable) {
-            voiceChannel.leave();
-            return message.channel.send(createEmbed("error", "I'm sorry, but I can't speak in this voice channel. make sure I have the proper permissions!"));
+        if (!(voiceChannel as VoiceChannel).speakable) {
+            return message.channel.send({ embeds: [createEmbed("error", "I'm sorry, but I can't speak in this voice channel. make sure I have the proper permissions!")] });
         }
     });
 }
