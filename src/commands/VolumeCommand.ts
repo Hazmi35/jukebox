b1ba835c44fac060179aa242e156b051b580328f
Hazmi35 <contact@hzmi.xyz>
2021-08-16 20:37:15 +0700

    feat(VolumeCommand): Migrate to Discord.js v13

--- src/commands/VolumeCommand.ts
+++ src/commands/VolumeCommand.ts
@@ -11,24 +11,29 @@ import { Message } from "discord.js";
     usage: "{prefix}volume [new volume]"
 })
 export class VolumeCommand extends BaseCommand {
-    // @isUserInTheVoiceChannel() // TODO: VolumeCommand is disabled for now.
-    // @isMusicQueueExists()
-    // @isSameVoiceChannel()
-    // public execute(message: Message, args: string[]): any {
-    //     let volume = Number(args[0]);
+    @isUserInTheVoiceChannel() // TODO: VolumeCommand is disabled for now.
+    @isMusicQueueExists()
+    @isSameVoiceChannel()
+    // TODO: Remove this if it's reeenabled.
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public execute(message: Message, args: string[]): any {
+        message.channel.send({ embeds: [createEmbed("warn", "⚠ Volume command is disabled. Please use the volume functionality in Discord client directly")] })
+            .catch(e => this.client.logger.error("VOLUME_COMMAND_ERR:", e));
 
-    //     if (isNaN(volume)) return message.channel.send({ embeds: [createEmbed("info", `📶 The current volume is ${message.guild!.queue!.volume.toString()}`)] });
+        // let volume = Number(args[0]);
 
-    //     if (volume < 0) volume = 0;
-    //     if (volume === 0) return message.channel.send({ embeds: [createEmbed("warn", "❗ Please pause the music player instead of setting the volume to \`0\`")] });
-    //     if (Number(args[0]) > this.client.config.maxVolume) {
-    //         return message.channel.send({
-    //             embeds: [createEmbed("warn", `❗ I can't set the volume above \`${this.client.config.maxVolume}\``)]
-    //         });
-    //     }
+        // if (isNaN(volume)) return message.channel.send({ embeds: [createEmbed("info", `📶 The current volume is ${message.guild!.queue!.volume.toString()}`)] });
 
-    //     message.guild!.queue!.volume = Number(args[0]);
-    //     message.guild!.queue!.player.setVolume(Number(args[0]) / this.client.config.maxVolume);
-    //     message.channel.send({ embeds: [createEmbed("info", `📶 Volume set to ${args[0]}`)] }).catch(console.error);
-    // }
+        // if (volume < 0) volume = 0;
+        // if (volume === 0) return message.channel.send({ embeds: [createEmbed("warn", "❗ Please pause the music player instead of setting the volume to \`0\`")] });
+        // if (Number(args[0]) > this.client.config.maxVolume) {
+        //     return message.channel.send({
+        //         embeds: [createEmbed("warn", `❗ I can't set the volume above \`${this.client.config.maxVolume}\``)]
+        //     });
+        // }
+
+        // message.guild!.queue!.volume = Number(args[0]);
+        // message.guild!.queue!.player.setVolume(Number(args[0]) / this.client.config.maxVolume);
+        // message.channel.send({ embeds: [createEmbed("info", `📶 Volume set to ${args[0]}`)] }).catch(console.error); //FIXME: why is there console here?
+    }
 }
