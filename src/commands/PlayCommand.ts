5b8fbe0324076ef24e7590bf993e2b6ef4942874
Hazmi35 <contact@hzmi.xyz>
2021-09-29 20:00:44 +0700

    fix(PlayCommand): Fix song selection doesn't trigger to number above 12

--- src/commands/PlayCommand.ts
+++ src/commands/PlayCommand.ts
@@ -127,7 +127,7 @@ export class PlayCommand extends BaseCommand {
                             if (message.author.id !== msg2.author.id) return false;
 
                             if (msg2.content === "cancel" || msg2.content === "c") return true;
-                            return Number(msg2.content) > 0 && Number(msg2.content) < 13;
+                            return Number(msg2.content) > 0 && Number(msg2.content) < (this.client.config.searchMaxResults + 1);
                         }, {
                             max: 1,
                             time: this.client.config.selectTimeout,
