a973159d58880f57ba14f132345e47e983b4128b
Hazmi35 <contact@hzmi.xyz>
2021-10-24 14:46:12 +0700

    feat(YouTubeTrack): Enhanced support for youtube live videos

--- src/structures/YouTubeTrack.ts
+++ src/structures/YouTubeTrack.ts
@@ -5,8 +5,11 @@ import { Track, TrackType } from "./Track";
 
 export class YouTubeTrack extends Track {
     public type = TrackType.youtube;
-    public readonly resourceFormat: string = "bestaudio/93";
+    public readonly resourceFormat: string = "bestaudio/140";
     public constructor(public readonly queue: ServerQueue, public readonly metadata: ITrackMetadata, public readonly inlineVolume: boolean = false) {
-        super(queue, metadata, inlineVolume);
+        super(queue, metadata, inlineVolume, {
+            downloaderArgs: "ffmpeg:-movflags frag_keyframe+empty_moov frag_keyframe+empty_moov -f opus",
+            extractorArgs: "youtube:include-live-dash"
+        });
     }
 }
