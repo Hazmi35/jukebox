6ab93763a24b5689806801a4384df1e7a4d68aca
Hazmi35 <contact@hzmi.xyz>
2021-11-18 07:31:05 +0000

    fix(YouTubeTrack): Fix downloaderArgs

--- src/structures/YouTubeTrack.ts
+++ src/structures/YouTubeTrack.ts
@@ -8,7 +8,7 @@ export class YouTubeTrack extends Track {
     public readonly resourceFormat: string = "bestaudio/140";
     public constructor(public readonly queue: ServerQueue, public readonly metadata: ITrackMetadata, public readonly inlineVolume: boolean = false) {
         super(queue, metadata, inlineVolume, {
-            downloaderArgs: "ffmpeg:-movflags frag_keyframe+empty_moov frag_keyframe+empty_moov -f opus",
+            downloaderArgs: '"ffmpeg:-movflags frag_keyframe+empty_moov frag_keyframe+empty_moov -f opus"',
             extractorArgs: "youtube:include-live-dash"
         });
     }
