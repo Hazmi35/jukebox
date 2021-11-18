e8cfa38391670c64f82b9eedc1d6bd7e09852518
Hazmi35 <contact@hzmi.xyz>
2021-11-18 20:02:55 +0700

    feat(YouTubeTrack): Use YouTube live dash mpd format

--- src/structures/YouTubeTrack.ts
+++ src/structures/YouTubeTrack.ts
@@ -7,7 +7,9 @@ export class YouTubeTrack extends Track {
     public type = TrackType.youtube;
     public constructor(public readonly queue: ServerQueue, public readonly metadata: ITrackMetadata, public readonly inlineVolume: boolean = false) {
         super(queue, metadata, inlineVolume, {
-            format: `${defaultYtFlags.format}/95`
+            format: `${defaultYtFlags.format}/95`,
+            downloaderArgs: "ffmpeg:-acodec libopus -f opus",
+            extractorArgs: "youtube:include-live-dash"
         });
     }
 }
