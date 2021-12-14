59ba74a4c7fbe72f6be6bbf0a9b761112919e318
Hazmi35 <contact@hzmi.xyz>
2021-12-15 00:30:13 +0700

    feat(config): Better parser (#912)

--- src/config.ts
+++ src/config.ts
@@ -1,42 +1,53 @@
 import { ActivitiesOptions } from "discord.js";
 
+function getValue(config: string, defaultValue: any): string {
+    const valueRaw = process.env[config];
+    if (valueRaw === undefined || valueRaw === "" || valueRaw === " ") return defaultValue;
+
+    return valueRaw.toString();
+}
+
+function getBoolean(config: string): boolean {
+    return process.env[config]?.toLowerCase() === "yes";
+}
+
 // Base config
-export const prefix = process.env.CONFIG_PREFIX?.replace(/"/g, "") ?? "jukebox "; // Temporary workaround for https://github.com/docker/compose/issues/6951
-export const owners: string[] = process.env.CONFIG_OWNERS?.replace(/  +/g, " ").split(/,[ ]?/) ?? [];
-export const totalShards: string | number = process.env.CONFIG_TOTALSHARDS?.toLowerCase() ?? "auto";
-export const debug = process.env.CONFIG_DEBUG?.toLowerCase() === "yes";
-export const cacheUsers = process.env.CONFIG_CACHE_USERS?.toLowerCase() === "yes";
+export const prefix = getValue("CONFIG_PREFIX", "jukebox ").replace(/"/g, ""); // Temporary workaround for https://github.com/docker/compose/issues/6951
+export const owners: string[] = getValue("CONFIG_OWNERS", "").replace(/  +/g, " ").split(/,[ ]?/);
+export const totalShards: string | number = getValue("CONFIG_TOTALSHARDS", "auto").toLowerCase();
+export const debug = getBoolean("CONFIG_DEBUG");
+export const cacheUsers = getBoolean("CONFIG_CACHE_USERS");
 
 if (totalShards !== "auto" && isNaN(totalShards as unknown as number)) throw new Error("CONFIG_TOTALSHARDS must be a number or \"auto\"");
 
 // Queue config
-export const allowDuplicate: boolean = process.env.CONFIG_ALLOW_DUPLICATE?.toLowerCase() === "yes";
-export const deleteQueueTimeout = Number(process.env.CONFIG_DELETE_QUEUE_TIMEOUT) * 1000 || 180 * 1000;
+export const allowDuplicate = getBoolean("CONFIG_ALLOW_DUPLICATE");
+export const deleteQueueTimeout = Number(getValue("CONFIG_DELETE_QUEUE_TIMEOUT", 180)) * 1000;
 
 // Play command config
-export const selectTimeout = Number(process.env.CONFIG_SELECT_TIMEOUT) * 1000 || 20 * 1000;
-export const disableTrackSelection = process.env.CONFIG_DISABLE_TRACK_SELECTION?.toLowerCase() === "yes";
-export const searchMaxResults = Number(process.env.CONFIG_SEARCH_MAX_RESULTS) || 10;
+export const selectTimeout = Number(getValue("CONFIG_SELECT_TIMEOUT", 20)) * 1000;
+export const disableTrackSelection = getBoolean("CONFIG_DISABLE_TRACK_SELECTION");
+export const searchMaxResults = Number(getValue("CONFIG_SEARCH_MAX_RESULTS", 10));
 
 if (searchMaxResults < 1) throw new Error("CONFIG_SEARCH_MAX_RESULTS cannot be smaller than 1");
 if (searchMaxResults > 15) throw new Error("CONFIG_SEARCH_MAX_RESULTS cannot be higher than 12");
 
 // Volume config
-export const enableInlineVolume = process.env.CONFIG_ENABLE_INLINE_VOLUME?.toLowerCase() === "yes";
-export const defaultVolume = Number(process.env.CONFIG_DEFAULT_VOLUME) || 100;
-export const maxVolume = Number(process.env.CONFIG_MAX_VOLUME) || 100;
+export const enableInlineVolume = getBoolean("CONFIG_ENABLE_INLINE_VOLUME");
+export const defaultVolume = Number(getValue("CONFIG_DEFAULT_VOLUME", 100));
+export const maxVolume = Number(getValue("CONFIG_MAX_VOLUME", 100));
 
 // Music cache config
 // TODO: Recreate cache system
-export const cacheYoutubeDownloads: boolean = process.env.CONFIG_CACHE_YOUTUBE_DOWNLOADS?.toLowerCase() === "yes";
-export const cacheMaxLengthAllowed = Number(process.env.CONFIG_CACHE_MAX_LENGTH) || 5400;
+export const cacheYoutubeDownloads = getBoolean("CONFIG_CACHE_YOUTUBE_DOWNLOADS");
+export const cacheMaxLengthAllowed = Number(getValue("CONFIG_CACHE_MAX_LENGTH", 5400));
 
 // Miscellaneous config
-export const disableInviteCmd = process.env.CONFIG_DISABLE_INVITE_CMD?.toLowerCase() === "yes";
+export const disableInviteCmd = getBoolean("CONFIG_DISABLE_INVITE_CMD");
 export const status = {
-    type: (process.env.CONFIG_STATUS_TYPE?.toUpperCase() ?? "LISTENING") as ActivitiesOptions["type"] | undefined,
-    activity: process.env.CONFIG_STATUS_ACTIVITY ?? "music on {guildsCount} guilds"
+    type: getValue("CONFIG_STATUS_TYPE", "LISTENING").toUpperCase() as ActivitiesOptions["type"],
+    activity: getValue("CONFIG_STATUS_ACTIVITY", "music on {guildsCount} guilds")
 };
 
 // i18n Config
-export const lang = process.env.CONFIG_LANGUAGE ?? "en-US";
+export const lang = getValue("CONFIG_LANGUAGE", "en-US");
