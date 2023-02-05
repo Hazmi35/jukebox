f3c301cfda29b2267bb8a3d0e652db90c2372b0c
Hazmi Alfarizqi <contact@hzmi.xyz>
2023-02-05 22:30:03 +0700

    chore: start over

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Set permissions for volumes
-chown -R jukebox:jukebox /app/logs /app/cache
-
-# Execute Docker's CMD as jukebox user
-exec sudo -E -u jukebox "$@"
--- src/structures/BaseEvent.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "discord.js";
-import { IEvent } from "../typings";
-
-export class BaseEvent implements IEvent {
-    public constructor(public readonly client: Client, public name: IEvent["name"]) {}
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function, class-methods-use-this
-    public execute(...args: any): void {}
-}
